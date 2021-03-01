796cd37c862d7f316268d6dbeb46111384a02969
MattIPv4 <me@mattcowley.co.uk>
2021-03-01 11:25:44 +0000

    Posting to statuspage works

--- src/index.js
+++ src/index.js
@@ -22,12 +22,45 @@ const jsonResponse = obj => new Response(JSON.stringify(obj), {
 const fetchLean20 = (monitor, region) => fetch(
     `https://api.lean20.com/v1/reporting/ping/activity/${monitor}?limit=10&location=${region}`,
     { headers: { access_token: process.env.LEAN20_AUTH } },
-);
+).then(req => req.json());
+
+// Post data to statuspage
+const postStatuspage = (page, metric, data) => fetch(
+    `https://api.statuspage.io/v1/pages/${page}/metrics/${metric}/data.json`,
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `OAuth ${process.env.STATUSPAGE_AUTH}`,
+        },
+        body: JSON.stringify({ data }),
+    },
+).then(req => req.json());
+
+// Get the average up latency from a set of results
+const averageLatency = results => {
+    const upLatencies = results.filter(res => res.result === 1).map(res => res.latency);
+    return upLatencies.reduce((sum, latency) => sum + latency, 0) / upLatencies.length;
+};
 
 // Fetch data for a monitor from lean20 and update statuspage metric
 const updateMetric = async metric => {
-    const allData = await Promise.all(metric.lean20.regions.map(region => fetchLean20(metric.lean20.monitor, region)));
-    console.log(JSON.stringify(allData, null, 2));
+    // Get the raw data from Lean20
+    const rawData = await Promise.all(metric.lean20.regions.map(region => fetchLean20(metric.lean20.monitor, region)));
+
+    // Flatten the data and group by minute
+    const groupedData = rawData.flat(1).reduce((acc, res) => {
+        const date = Math.floor(new Date(res.checked_at).getTime() / 1000 / 60);
+        acc[date] = (acc[date] || []).concat(res);
+        return acc;
+    }, {});
+
+    // Send to statuspage
+    await Promise.all(Object.entries(groupedData).map(([minute, results]) => postStatuspage(
+        metric.statuspage.page,
+        metric.statuspage.metric,
+        { timestamp: minute * 60, value: averageLatency(results) },
+    )));
 };
 
 // Fetch all lean20 monitors and update statuspage metrics
